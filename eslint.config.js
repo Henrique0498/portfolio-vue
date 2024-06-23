fd1aa9113262b90149edafb9c916f06673de7981
Henrique Lopes <h.lopes.silva2015@gmail.com>
2024-06-23 01:48:14 -0300

    removing comment from the eslint file

--- eslint.config.js
+++ eslint.config.js
@@ -1,35 +1,3 @@
-/*import { createConfigForNuxt } from '@nuxt/eslint-config/flat'
-
-export default createConfigForNuxt({
-  modules: ['@nuxt/eslint']
-  })
-  .prepend
-  // ...Prepend some flat configs in front
-  ()
-  // Override some rules in a specific config, based on their name
-  .override('nuxt/typescript', {
-    rules: {
-      // ...Override rules, for example:
-      '@typescript-eslint/ban-types': 'off'
-    }
-  })
-
-*/
-
-// export default eslint
-/*
-rules: {
-  'vue/no-unused-vars': 'off',
-  'no-unused-vars': [
-    'error',
-    {
-      varsIgnorePattern: '^_',
-      argsIgnorePattern: '^_',
-      ignoreRestSiblings: true,
-      },
-      ],
-      'vue/multi-word-component-names': 'off',
-      */
 import { createConfigForNuxt } from '@nuxt/eslint-config/flat'
 
 export default createConfigForNuxt({
