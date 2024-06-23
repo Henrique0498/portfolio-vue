bb2e6ba2b9cf8c93bffb2fac6a58384bb2d2d5d7
Henrique Lopes <h.lopes.silva2015@gmail.com>
2024-06-23 15:25:58 -0300

    Add: Nuxt Icons

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -7,8 +7,10 @@ import _Typography from '~/components/Typography/Typography.vue'
     class="w-full h-full flex flex-col p-4 justify-center items-center text-emerald-950 bg-gradient-to-tr from-emerald-500 to-teal-700"
   >
     <Typography class="title">Em construção</Typography>
-    <p>Projeto em NuxtJS</p>
-    <UDivider class="max-w-xl" />
+    <p>Este projeto é feito em NuxtJS</p>
+    <UDivider class="max-w-xl">
+      <Icon name="ri:tools-line" class="text-emerald-950 w-8 h-8" />
+    </UDivider>
     <ULink to="https://henriquelopes.dev.br" class="hover:text-black">
       Ir para o projeto feito em NextJS
     </ULink>
