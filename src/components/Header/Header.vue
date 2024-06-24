fab10bda9d0ac12d0115958e1810dea2154c8e07
Henrique Lopes <h.lopes.silva2015@gmail.com>
2024-06-24 01:50:38 -0300

    Create: Sidebar mobile

--- src/components/Header/Header.vue
+++ src/components/Header/Header.vue
@@ -1,40 +1,65 @@
 <template>
-  <nav ref="header" class="navContainer">
+  <nav ref="header" class="navContainer" :class="classBackground">
     <header>
       <Logo />
 
-      <nav>
+      <button class="bugger md:hidden" @click="isOpen = !isOpen">
+        <span class="text">Abrir menu</span>
+        <span class="icon" :class="[isOpen ? 'open' : '']"></span>
+      </button>
+
+      <nav class="hidden md:block">
         <ul>
           <li>
             <p title="Em construção" class="line-through cursor-not-allowed">
               Projetos
             </p>
           </li>
-          <li>
-            <NavLink
-              href="https://www.linkedin.com/in/henrique-ls/"
-              target="__blank"
-              >LinkedIn</NavLink
-            >
-          </li>
-          <li>
-            <NavLink href="https://henriquelopes.dev.br" target="__blank"
-              >Versão em NextJs</NavLink
-            >
+          <li v-for="link in links" :key="link.id">
+            <NavLink :href="link.href" target="__blank">{{
+              link.label
+            }}</NavLink>
           </li>
         </ul>
       </nav>
     </header>
-    <slot />
   </nav>
+
+  <transition name="sidebar">
+    <nav v-if="isOpen" class="sidebar md:hidden" :class="classBackground">
+      <ul class="list">
+        <li>
+          <p title="Em construção" class="line-through cursor-not-allowed">
+            Projetos
+          </p>
+        </li>
+        <li v-for="link in links" :key="link.id">
+          <NavLink :href="link.href" target="__blank">{{ link.label }}</NavLink>
+        </li>
+      </ul>
+    </nav>
+  </transition>
 </template>
 
 <script setup lang="ts">
 import { ref, onMounted } from 'vue'
 import Logo from '../Logo/Logo.vue'
-const header = ref<HTMLHeadingElement | null>(null)
 
 const colorMode = useColorMode()
+const isOpen = ref(false)
+const classBackground = ref('')
+const links = ref([
+  {
+    id: 'linkedIn',
+    href: 'https://www.linkedin.com/in/henrique-ls/',
+    label: 'LinkedIn'
+  },
+  {
+    id: 'projectNextJS',
+    href: 'https://henriquelopes.dev.br',
+    label: 'Projeto em NextJS'
+  }
+])
 
 computed({
   get() {
@@ -48,19 +73,23 @@ computed({
 function handleScroll() {
   const scrollHeight = window.scrollY
 
-  if (
-    header.value?.className &&
-    scrollHeight &&
-    !header.value.className.includes('dark')
-  ) {
-    header.value.className += ' dark'
-  } else if (header.value?.className && !scrollHeight) {
-    header.value.className = header.value.className.replace(' dark', '')
+  if (scrollHeight) {
+    classBackground.value = 'dark'
+  } else if (!scrollHeight) {
+    classBackground.value = ''
+  }
+}
+
+function handleResize() {
+  if (isOpen.value && window.innerWidth >= 768) {
+    isOpen.value = false
   }
 }
 
 onMounted(() => {
   handleScroll()
+
+  window.addEventListener('resize', handleResize)
 
   window.addEventListener('scroll', handleScroll)
 })
