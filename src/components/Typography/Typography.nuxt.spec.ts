17ea4eba5d479c03cc404c65e7213e1b5af99bd2
Henrique Lopes <h.lopes.silva2015@gmail.com>
2024-06-27 16:08:33 -0300

    Remove: Component Typography

--- src/components/Typography/Typography.nuxt.spec.ts
+++
@@ -1,9 +0,0 @@
-import { mountSuspended } from '@nuxt/test-utils/runtime'
-
-import Title from './Typography.vue'
-import { expect, it } from 'vitest'
-
-it('<Typography/>', async () => {
-  const component = await mountSuspended(Title)
-  expect(component).toBeDefined()
-})
